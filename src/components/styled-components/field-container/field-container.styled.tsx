521155a2c1665b65a8b4bf6d3da5655ce1cfbae0
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-08 17:49:05 +0300

    * fix styles

--- src/components/styled-components/field-container/field-container.styled.tsx
+++ src/components/styled-components/field-container/field-container.styled.tsx
@@ -1,8 +1,10 @@
+import { SPACES } from 'src/theme';
 import styled from 'styled-components';
 
 const FieldContainer = styled.div`
   display: flex;
   flex-direction: column;
+  gap: ${SPACES.S};
 `;
 
 export { FieldContainer };
--- src/components/styled-components/field-label/field-label.styled.tsx
+++ src/components/styled-components/field-label/field-label.styled.tsx
@@ -1,8 +1,7 @@
-import { COLORS, FONTS, SPACES } from 'src/theme';
+import { COLORS, FONTS } from 'src/theme';
 import styled from 'styled-components';
 
 const FieldLabel = styled.label`
-  margin-bottom: ${SPACES.S};
   color: ${COLORS.GRAY};
   font-weight: ${FONTS.WEIGHTS.MEDIUM};
   font-size: ${FONTS.SIZES.M};
