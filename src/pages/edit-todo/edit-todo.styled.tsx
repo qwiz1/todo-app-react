3db5a7b4a45fbfb670260c74b1bd50bd3f83f27c
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-08 18:00:07 +0300

    + edit todo page

---
+++ src/pages/edit-todo/edit-todo.styled.tsx
@@ -0,0 +1,56 @@
+import { Form } from 'formik';
+import { SPACES } from 'src/theme';
+import styled from 'styled-components';
+
+const EditTodoMain = styled.main`
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${SPACES.L} 0;
+`;
+
+const EditForm = styled(Form)`
+  min-height: 80vh;
+  max-width: 640px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${SPACES.S};
+`;
+
+const SwitchesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${SPACES.S};
+`;
+
+const SwitchContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ButtonGroup = styled.div`
+  max-width: 400px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export {
+  EditTodoMain,
+  EditForm,
+  FieldContainer,
+  SwitchesContainer,
+  SwitchContainer,
+  ButtonGroup,
+};
