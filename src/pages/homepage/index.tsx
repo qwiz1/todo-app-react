2e20515afc856a1725a4f1e8a56ff5e4b2988ace
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-08 17:58:24 +0300

    + app header

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from 'src/components/header';
 
 const Homepage: React.FC = () => {
   return (
-    <main>
-      <div>Homepage</div>
+    <>
+      <Header />
       <Outlet />
-    </main>
+    </>
   );
 };
 
