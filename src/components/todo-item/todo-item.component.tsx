84e6c02c6fe2041dd2ec39eafceeb6870c5d7543
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-26 19:26:48 +0300

    * using own components instead material ui

--- src/components/todo-item/todo-item.component.tsx
+++ src/components/todo-item/todo-item.component.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Box, Switch } from '@mui/material';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Typography from '@mui/material/Typography';
 
 import { Todo } from 'src/common/types';
-import { TodoItemWrapper, TodoDescriptionCard } from './todo-item.styled';
 import { AppRoute } from 'src/common/enums';
 import { SmallButton } from '../styled-components/button';
 import { useDeleteTodo, useUpdateTodo } from 'src/query-hooks';
 import { toast } from 'react-toastify';
+import {
+  BtnWrapper,
+  CardActions,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  TodoCard,
+} from './todo-item.styled';
+import { Switch } from '@mui/material';
 
 type Props = {
   todo: Todo;
@@ -37,46 +44,21 @@ const TodoItem: React.FC<Props> = ({ todo }) => {
   };
 
   return (
-    <TodoItemWrapper>
-      <Card
-        sx={{
-          borderRadius: 0,
-          boxShadow: 'none',
-          border: '4px solid black',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '250px',
-          maxWidth: '372px',
-          margin: '0 auto',
-        }}
-      >
-        <CardContent
-          sx={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            paddingBottom: 0,
-          }}
-        >
-          <Typography gutterBottom variant="h4" component="div">
-            {todo.title}
-          </Typography>
-          <TodoDescriptionCard isCompleted={isCompleted}>
-            {todo.description}
-          </TodoDescriptionCard>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            <Link to={`${AppRoute.TODOS}/${todo.id}`}>
-              <SmallButton>View</SmallButton>
-            </Link>
-            <SmallButton onClick={handleOnClickDelete}>Delete</SmallButton>
-          </Box>
-          <Switch checked={isCompleted} onChange={handleSwitchChange} />
-        </CardActions>
-      </Card>
-    </TodoItemWrapper>
+    <TodoCard>
+      <CardContent>
+        <CardTitle>{todo.title}</CardTitle>
+        <CardDescription done={isCompleted}>{todo.description}</CardDescription>
+      </CardContent>
+      <CardActions>
+        <BtnWrapper>
+          <Link to={`${AppRoute.TODOS}/${todo.id}`}>
+            <SmallButton>View</SmallButton>
+          </Link>
+          <SmallButton onClick={handleOnClickDelete}>Delete</SmallButton>
+        </BtnWrapper>
+        <Switch checked={isCompleted} onChange={handleSwitchChange} />
+      </CardActions>
+    </TodoCard>
   );
 };
 
--- src/components/todo-item/todo-item.styled.tsx
+++ src/components/todo-item/todo-item.styled.tsx
@@ -1,15 +1,56 @@
 import styled from 'styled-components';
-import { SPACES } from 'src/theme';
-import { TodoDescription } from '../styled-components/todo-description';
+import { COLORS, SPACES } from 'src/theme';
+import { SIZES } from 'src/theme/fonts.const';
 
-const TodoItemWrapper = styled.section`
-  margin-top: ${SPACES.M};
+const TodoCard = styled.div`
+  padding: ${SPACES.S} ${SPACES.S} 0;
+  margin: ${SPACES.ML} auto;
+  max-width: 380px;
+  height: 260px;
+  border: 4px solid ${COLORS.DARK};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-const TodoDescriptionCard = styled(TodoDescription)`
+const CardContent = styled.div`
+  padding: ${SPACES.S};
+  overflow-y: auto;
+
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const CardTitle = styled.h3`
+  margin-top: 0;
+  font-size: ${SIZES.L};
+  font-weight: 500;
+`;
+
+const CardDescription = styled.p<{ done: boolean }>`
   margin: 0;
-  max-height: 118px;
-  overflow: auto;
+  text-decoration: ${(props) => (props.done ? 'line-through' : 'none')};
 `;
 
-export { TodoItemWrapper, TodoDescriptionCard };
+const CardActions = styled.div`
+  padding: calc(${SPACES.S} / 2) 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const BtnWrapper = styled.div`
+  display: flex;
+  gap: ${SPACES.S};
+`;
+
+export {
+  TodoCard,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardActions,
+  BtnWrapper,
+};
